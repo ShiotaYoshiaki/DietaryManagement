d448c99b7344a7a104c7dc0034f1dd4dbdf78b4c
ShiotaYoshiaki <ohayoppe-yoshi@outlook.jp>
2020-06-09 22:12:17 +0900

    loginをデモ用に作成

--- FrontSide/routes/BottomTab.js
+++ FrontSide/routes/BottomTab.js
@@ -2,10 +2,12 @@ import Home from "../components/screens/Home.vue";
 import Record from "../components/screens/Record.vue";
 import Nutrients from "../components/screens/Nutrients.vue";
 import Settings from "../components/screens/Settings.vue";
+import Login from "../components/screens/Login.vue";
 
 export default {
   Home: Home,
   Record: Record,
   Nutrients: Nutrients,
   Settings: Settings,
+  Login: Login,
 };
