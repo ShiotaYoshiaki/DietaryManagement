658205827b6177712a8a197346d2d51dec984522
EisukeSasaki <eisu_sasa604@icloud.com>
2020-06-20 16:03:53 +0900

    ユーザー情報とテーブルの追加

--- FrontSide/components/screens/Settings.vue
+++ FrontSide/components/screens/Settings.vue
@@ -1,7 +1,29 @@
 <template>
   <view class="container">
-    <text class="heading">Settings</text>
-    <text>This is the App Settings screen</text>
+    <view class="title"> </view>
+    <view>
+      <text class="info">ユーザー情報</text>
+      <text>ユーザーネーム：</text>
+      <text>生年月日：</text>
+      <text>体重：</text>
+      <text>身長：</text>
+      <text>BMI：</text>
+      <text>性別：</text>
+    </view>
+    <view class="ranking_container">
+      <view class="lack_Ranking">
+        <text>足りていない栄養素TOP3</text>
+        <view class="lack_Ranking"></view>
+        <view></view>
+        <view></view>
+      </view>
+      <view class="over_Ranking">
+        <text>摂りすぎな栄養素TOP3</text>
+        <view></view>
+        <view></view>
+        <view></view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -17,4 +39,40 @@
   color: darkolivegreen;
   margin: 20px;
 }
+
+.title {
+  font-size: 30px;
+}
+
+.info {
+  font-size: 30px;
+  font-weight: bold;
+  color: darkolivegreen;
+  margin: 20px;
+}
+
+
+/*足りてない栄養素テーブル*/
+.lack_Ranking {
+  border-color: #000;
+  border-style: solid;
+  border-width: 1px;
+}
+
+/*取りすぎな栄養素テーブル*/
+.over_Ranking {
+  border-color: #000;
+  border-style: solid;
+  border-width: 1px;
+}
+
+/*ランキングのコンテイナー*/
+.ranking_container {
+  flex: 1;
+  flex-direction: row;
+}
+
+
+
+
 </style>
