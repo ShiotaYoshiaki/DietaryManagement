b5f06fa8e24f4b658e54d34f064950dcafe35dac
Kato Shuho <showing838@gmail.com>
2020-06-21 16:26:28 +0900

    マイ自炊ｾｯﾄ更新

--- FrontSide/components/parts/serving.vue
+++ FrontSide/components/parts/serving.vue
@@ -1,7 +1,5 @@
 <template>
   <view>
     <text>〇人前と書く</text>
-    <text type="number"></text>
-    
   </view>
 </template>
--- FrontSide/store/index.js
+++ FrontSide/store/index.js
@@ -1,11 +1,18 @@
-import Vue from 'vue-native-core'
-import Vuex from 'vuex'
-import messages from './modules/messages'
-
-Vue.use(Vuex)
+import Vue from 'vue-native-core';
+import Vuex from 'vuex';
+import messages from './modules/messages';
+import { VueNativeBase } from "native-base";
+// registering all native-base components to the global scope of the Vue
+Vue.use(VueNativeBase);
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     messages
   }
 });
+
+new Vue({
+  VueNativeBase,
+  render: h => h(App),
+}).$mount('#app')
