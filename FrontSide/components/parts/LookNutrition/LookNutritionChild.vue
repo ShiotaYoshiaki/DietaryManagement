dcfa19a2662dfb49ff2421d21da626f76522b628
Kato Shuho <showing838@gmail.com>
2020-06-26 21:41:40 +0900

    不要なﾌｧｲﾙを削除

--- FrontSide/components/parts/LookNutrition/LookNutritionChild.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <view>
-    <slot title="toLookNutrition"></slot>
-    <slot title="toLookNutrition"></slot>
-    <slot title="toLookNutrition"></slot>
-    <slot title="toLookNutrition"></slot>
-  </view>
-</template>
-
--- FrontSide/components/parts/checkBox/checkboxedChild.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <view>
-    <slot title="choice"></slot>
-    <slot title="choice2"></slot>
-  </view>
-</template>
