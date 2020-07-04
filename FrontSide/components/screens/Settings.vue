f4e8a4daa1a5208d152b1a0d7b68194fc8a3bcc9
EisukeSasaki <eisu_sasa604@icloud.com>
2020-07-04 14:13:47 +0900

    テーブルの作成

--- FrontSide/components/screens/Settings.vue
+++ FrontSide/components/screens/Settings.vue
@@ -1,39 +1,41 @@
 <template>
   <view class="container">
-    <view class="title"> </view>
+    <text class="title">ユーザー情報</text>
     <view class="info-wrapper">
-      <text class="info">ユーザー情報</text>
-      <text>ユーザーネーム：</text>
-      <text>生年月日：</text>
-      <text>体重：</text>
-      <text>身長：</text>
-      <text>BMI：</text>
-      <text>性別：</text>
+      <view><nb-text class="info-details">ユーザーネーム：</nb-text></view>
+      <view><nb-text class="info-details">生年月日：</nb-text></view>
+      <view><nb-text class="info-details">体重：</nb-text></view>
+      <nb-text class="info-details">身長：</nb-text>
+      <nb-text class="info-details">BMI：</nb-text>
+      <nb-text class="info-details">性別：</nb-text>
     </view>
+    <!-- <nb-button block primary>
+      <nb-text>Primary</nb-text>
+    </nb-button> -->
     <view class="ranking_container">
       <view class="lack_Ranking">
-        <text>足りていない栄養素TOP3</text>
-        <view></view>
-        <view></view>
-        <view></view>
+        <text>足りていない栄養素TOP3</text>   
+        <view><text class="ranking_border">テスト１</text></view>
+        <view><text class="ranking_border">テスト２</text></view>
+        <view><text class="ranking_border">テスト３</text></view>
       </view>
       <view class="over_Ranking">
         <text>摂りすぎな栄養素TOP3</text>
-        <view></view>
-        <view></view>
-        <view></view>
+        <view><text class="ranking_border"></view>
+        <view><text class="ranking_border"></view>
+        <view><text class="ranking_border"></view>
       </view>
     </view>
     <view class="details_container">
-      <view class="checking_nutrients">
-        <text>摂取栄養素を見る</text>
+      <view>
+        <nb-text class="checking_nutrients">摂取栄養素を見る</nb-text>
       </view>
-      <view class="changeing_user_info">
-        <text>ユーザー情報を変更する</text>
+      <view>
+        <text class="changeing_user_info">ユーザー情報を変更する</text>
       </view>
     </view>
-  <view>
-    <text class="home_button">←</text>
+  <view class="home_button">
+    <nb-button><text color="red">←</text></nb-button>
   </view>
   </view>
 </template>
@@ -51,27 +53,25 @@
   margin: 20px;
 }
 
+
 .title {
-  font-size: 30px;
-}
-
-
-.info {
   font-size: 30px;
   font-weight: bold;
   color: darkolivegreen;
   margin: 20px;
 }
 .info-wrapper {
-  margin-left: 40px;
+  margin-left: 30px;
+
 }
 
-
+.info-details {
+  line-height: 30;
+}
 
 
 /*ランキングのコンテイナー*/
 .ranking_container {
-  /* flex: 1; */
   flex-direction: row;
   justify-content: center;
 }
@@ -81,10 +81,21 @@
   border-color: #000;
   border-style: solid;
   border-width: 1px;
-  margin-top: 30px;
-  margin-right: 20px;
   width: 160px;
   text-align: center;
+  margin-top: 30px;
+  margin-right: 20px;
+  /* background-color: red; */
+ 
+}
+
+/*ランキングテーブルボーダー*/
+.ranking_border {
+  height: 30;
+  border-color: black;
+  border-style: solid;
+  border-width: 0.5px;
+  /* border-bottom-width: 1px; */
 }
 
 /*取りすぎな栄養素テーブル*/
@@ -95,6 +106,7 @@
   margin-top: 30px;
   width: 160px;
   text-align: center;
+  /* border-collapse: collapse; */
 }
 
 /*詳細情報コンテイナー*/
@@ -113,6 +125,7 @@
   margin-right: 20px;
   width: 160px;
   text-align: center;
+  font-size: 15px;
 }
 
 /*ユーザー情報を変更する*/
@@ -123,6 +136,8 @@
   margin-top: 30px;
   margin-right: 20px;
   width: 160px;
+  text-align: center;
+  font-size: 15px;
 }
 
 
@@ -132,6 +147,8 @@
   margin-top: 60px;
   margin-left: 20px;
 }
+
+
 
 
 
