6d8e0192b9dc8b1fcd15fd69d6a0164070c775e0
Kato Shuho <showing838@gmail.com>
2020-07-08 00:11:44 +0900

    機能実装onchangeの挙動がおかしい

--- FrontSide/components/parts/searchBox.vue
+++ FrontSide/components/parts/searchBox.vue
@@ -1,8 +1,33 @@
 <template>
   <view class="seachBox">
-    <TextInput placeholder="検索欄" />
+    <TextInput
+      placeholder="検索欄"
+      v-model="keyword"
+      :onChange="keywordFunc"
+    />
   </view>
 </template>
+<script>
+export default {
+  props: {
+    cooks: {
+      type: Array,
+    },
+    test: {
+      type: Array,
+    },
+    keyword: {
+      type: String,
+      default: "dafault",
+    },
+  },
+  methods: {
+    keywordFunc() {
+      this.$emit("serchFunc");
+    },
+  },
+};
+</script>
 <style modules>
 .seachBox {
   width: 50%;
