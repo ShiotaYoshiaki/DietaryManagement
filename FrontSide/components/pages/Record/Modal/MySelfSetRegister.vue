2587cf34818ae3978414c362878fe0bf558c39a6
tanaka-yuu <tanaka07yuu23@gmail.com>
2020-07-10 22:51:06 +0900

    各要素のファイルをインポート

--- FrontSide/components/pages/Record/Modal/MySelfSetRegister.vue
+++ FrontSide/components/pages/Record/Modal/MySelfSetRegister.vue
@@ -3,6 +3,14 @@
 </template>
 
 <script>
+import MenuNameTextbox from "../../../parts/Textbox/MenuNameTextbox";
+import MenuList from "../../../parts/List/MenuList";
+import MenuRegisterButton from "../../../parts/Button/MenuRegisterButton";
+import LookNutritionListButton from "../../../parts/Button/LookNutritionListButton";
+import MenuNameTextbox from "../../../parts/Textbox/MenuNameTextbox";
+
+
+
 export default {
   
 }
