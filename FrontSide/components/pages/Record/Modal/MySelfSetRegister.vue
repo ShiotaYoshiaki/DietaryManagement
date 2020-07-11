6783f9f624a1cce7e38616f48481690ccae4c8b3
tanaka-yuu <tanaka07yuu23@gmail.com>
2020-07-11 09:21:47 +0900

    2回インポートしていたところを削除

--- FrontSide/components/pages/Record/Modal/MySelfSetRegister.vue
+++ FrontSide/components/pages/Record/Modal/MySelfSetRegister.vue
@@ -1,3 +1,4 @@
+// マイ自炊セット登録 (作成者 : 田中)
 <template>
   <text>MySelfSetRegister</text>
 </template>
@@ -7,7 +8,6 @@ import MenuNameTextbox from "../../../parts/Textbox/MenuNameTextbox";
 import MenuList from "../../../parts/List/MenuList";
 import MenuRegisterButton from "../../../parts/Button/MenuRegisterButton";
 import LookNutritionListButton from "../../../parts/Button/LookNutritionListButton";
-import MenuNameTextbox from "../../../parts/Textbox/MenuNameTextbox";
 
 
 
