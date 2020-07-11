ef5d7ea92c128236d4dceb8a7287a465ad655942
tanaka-yuu <tanaka07yuu23@gmail.com>
2020-07-11 09:54:33 +0900

    MenuNameTextboxの形は作った

--- FrontSide/components/parts/Textbox/MenuNameTextbox.vue
+++ FrontSide/components/parts/Textbox/MenuNameTextbox.vue
@@ -1,1 +1,40 @@
 // マイ自炊セット登録の「献立名」のテキストボックス
+<template>
+  <view class="flex m20">
+    <view class="menuName">
+      <text>献立名 : </text>
+    </view>
+    <view class="menuNameTextbox">
+      <TextInput placeholder="入力してください" />
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style modules>
+.flex {
+  flex-direction: row;
+}
+
+.m20 {
+  margin: 20px;
+}
+
+.menuName {
+  
+}
+
+.menuNameTextbox {
+  width: 50%;
+  height: 30;
+  border-color: #000;
+  border-style: solid;
+  border-width: 1px;
+}
+
+</style>
