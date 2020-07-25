aed509af216fe3a1669ba9f80bc8e072a4c27661
Kato Shuho <showing838@gmail.com>
2020-07-25 11:05:50 +0900

    git chery-pick抜け出した

--- FrontSide/components/pages/Record/Modal/MySelfList.vue
+++ FrontSide/components/pages/Record/Modal/MySelfList.vue
@@ -1,6 +1,5 @@
 <template>
   <view>
-<<<<<<< HEAD
     <nb-text>MyselfList</nb-text>
     <MyCateringListModal :open_func="open_func" :testRecord="testRecord" />
   </view>
@@ -20,16 +19,4 @@ export default {
     },
   },
 };
-=======
-   <CateringSetList />
-  </view>
-</template>
-<script>
-import CateringSetList from "../CateringSetList";
-export default {
-  components:{
-    CateringSetList,
-  }
-}
->>>>>>> e7576fd... 新しくマイ自炊ｾｯﾄのｺｰﾄﾞを作る
 </script>
