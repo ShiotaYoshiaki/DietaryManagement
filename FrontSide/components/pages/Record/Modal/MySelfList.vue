668200cc7e61009534135da145f241c79a417cea
Kato Shuho <showing838@gmail.com>
2020-07-25 11:02:51 +0900

    git cherypick前の二つlogをreset

--- FrontSide/components/pages/Record/Modal/MySelfList.vue
+++ FrontSide/components/pages/Record/Modal/MySelfList.vue
@@ -1,5 +1,6 @@
 <template>
   <view>
+<<<<<<< HEAD
     <nb-text>MyselfList</nb-text>
     <MyCateringListModal :open_func="open_func" :testRecord="testRecord" />
   </view>
@@ -19,4 +20,16 @@ export default {
     },
   },
 };
+=======
+   <CateringSetList />
+  </view>
+</template>
+<script>
+import CateringSetList from "../CateringSetList";
+export default {
+  components:{
+    CateringSetList,
+  }
+}
+>>>>>>> e7576fd... 新しくマイ自炊ｾｯﾄのｺｰﾄﾞを作る
 </script>
---
+++ FrontSide/components/parts/searchBox.vue
@@ -0,0 +1,62 @@
+<template>
+  <view class="seachBox">
+    <text-input
+      placeholder="検索欄"
+      v-model="keyword"
+      class="text_input"
+      :on-change="(e) => changeKeyword(e)"
+    />
+    <touchable-opacity class="serchBox_button" :on-press="keywordFunc"
+      ><text class="button_text">検索</text></touchable-opacity
+    >
+  </view>
+</template>
+<script>
+export default {
+  props: {
+    cooks: {
+      type: Array,
+    },
+    test: {
+      type: Array,
+    },
+    keyword: {
+      type: String,
+      default: "dafault",
+    },
+    changeKeyword: {
+      type: Function,
+    },
+  },
+  methods: {
+    keywordFunc() {
+      this.$emit("serchFunc");
+    },
+  },
+};
+</script>
+<style modules>
+.seachBox {
+  flex-direction: row;
+  width: 70%;
+  border-width: 1px;
+}
+.serchBox_button {
+  background-color: #008080;
+  align-items: center;
+  justify-content: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+.text_input {
+  background-color: white;
+  margin-right: 5px;
+  flex: 3;
+
+  width: 100%;
+}
+.button_text {
+  color: white;
+  font-weight: normal;
+}
+</style>
