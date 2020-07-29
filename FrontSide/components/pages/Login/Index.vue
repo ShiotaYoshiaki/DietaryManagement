49b43451ea150bd62b841796a9e88a147cdd6636
nemotoshogo <shota0701nemoto@gmail.com>
2020-07-29 23:46:37 +0900

    ログイン画面のデザインを実装

--- FrontSide/components/pages/Login/Index.vue
+++ FrontSide/components/pages/Login/Index.vue
@@ -1,3 +1,25 @@
 <template>
-    <text>Write me</text>
+
+<view>
+<text>メールアドレス</text>
+<textInput type="text" v-model="textInput1"/>
+
+<text>パスワード</text>
+<textInput type="text" v-model="textInput2"/>
+</view>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      textInput1: '入力',
+      textInput2: '入力',
+    }
+  },
+  styleObject: {
+    color: 'red',
+    fontSize: '13px'
+  }
+}
+</script>
