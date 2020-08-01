b3bc3aa50634a8b7188d47fe5525054124bde729
tanaka-yuu <tanaka07yuu23@gmail.com>
2020-08-01 09:29:48 +0900

    仮のユーザー情報を用意

---
+++ FrontSide/constants/TestUserInfo.js
@@ -0,0 +1,14 @@
+export const TEST_USER = {
+  key: "test",
+  mail: "test@gmail.com",
+  password: "testuser123",
+  username: "Mr.test",
+  birthday: {
+    year: 2000,
+    month: 1,
+    day: 1,
+  },
+  height: 170,
+  weight: 60,
+  sex: "Man",
+};
