6f4a8440ce852a362d0fa4a3792419cfe959923b
ShiotaYoshiaki <shiota.yosiaki.0904@gmail.com>
2020-08-01 22:38:55 +0900

    urlに従って認証するAPI構築

--- API/Account/src/service/login.js
+++ API/Account/src/service/login.js
@@ -1,21 +1,55 @@
+import UserInfo from "../dao/userInfo";
 
-export default async (param) => {
+function isEmptyObject(data) {
+  if (!data) return true;
+  if (!Object.keys(data).length) return true;
+  return false;
+}
+
+async function getTargetUser(name) {
   try {
-    const {
-      name, pass,
-    } = param;
-    const result = 'success';
-    const message = '';
-    return {
-      result,
-      message,
-      conform: {
-        name,
-        pass,
-      }
-    };
+    const repository = new UserInfo();
+    const result = await repository.findByName(name);
+    if (isEmptyObject(result)) {
+      throw new Error(`No exist user. name: ${name}`);
+    }
+    return result;
   } catch (e) {
-    console.log(e);
+    console.log("Error at getTargetUser");
+    throw e;
   }
 }
 
+function toIsMatchPass(userInfo, pass) {
+  const { pass: targetPass } = userInfo;
+  console.log(userInfo.pass);
+  console.log(pass);
+  return targetPass === pass;
+}
+
+async function login(param) {
+  const { name, pass } = param;
+  const userInfo = await getTargetUser(name);
+  const isMatch = toIsMatchPass(userInfo, pass);
+  return isMatch;
+}
+
+export default async (param) => {
+  try {
+    console.log(`Start login.js. param: ${JSON.stringify(param)}`);
+    const result = "success";
+    const message = "";
+    const conformResult = await login(param);
+    return {
+      result,
+      message,
+      conform: conformResult,
+    };
+  } catch (e) {
+    return {
+      result: "failed",
+      message: e,
+      conform: false,
+    };
+  }
+};
