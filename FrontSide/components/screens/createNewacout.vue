441fddbe2ea8c310949416cc011edf919c53070f
EisukeSasaki <eisu_sasa604@icloud.com>
2020-08-08 11:10:42 +0900

    メールアドレス画面編集

--- FrontSide/components/screens/createNewacout.vue
+++ FrontSide/components/screens/createNewacout.vue
@@ -2,7 +2,7 @@
   <nb-view class="container">
     <nb-text>確認メールに記載のワンタイムパスワード</nb-text>
     <nb-text class="text_center">を入力してください</nb-text>
-    <nb-view>
+    <nb-view class="L_pass">
       <nb-input class="onetime_pass" placeholder="キーワードを入力" type="text">
       </nb-input>
     </nb-view>
@@ -16,30 +16,42 @@
 
 <style>
 .container {
-  background-color: red;
   width: 300px;
-  margin-top: 200px;
+  margin-top: 150px;
   margin-bottom: 200px;
   margin-right: 50px;
   margin-left: 40px;
+  padding-top:30px;
+  padding-bottom:30px;
+  border-style: solid;
+  border-color: black;
+  border-width: 1px;
 }
 
 .text_center {
   text-align: center;
 }
 
+.L_pass {
+  height: 130px;
+}
+
 .onetime_pass {
   text-align: center;
-  background-color: blue;
   margin-top: 20px;
   margin-bottom: 20px;
   padding: 30px;
+  border-style: solid;
+  border-color: black;
+  border-width: 1px;
 }
 
 .confirm_button {
-  background-color: blueviolet;
-  width: 60px;
-  margin-left: 170px;
+  width: 65px;
+  margin-left: 190px;
+  border-style: solid;
+  border-color: black;
+  border-width: 1px;
 }
 
 .confirm_text {
