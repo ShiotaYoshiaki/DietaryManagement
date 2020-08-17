a3b482e3678d52824cfc9a0645c3dfac88bc6a8b
nemotoshogo <shota0701nemoto@gmail.com>
2020-08-17 23:38:50 +0900

    保存をしていなかったので、もう一度push

--- FrontSide/components/pages/Login/Index.vue
+++ FrontSide/components/pages/Login/Index.vue
@@ -1,38 +1,39 @@
 <template>
-  <view class="container">
-    <text class="text-color-primary">メールアドレス</text>
-    <textInput type="text" v-model="e-mailadress"/>
-    <text class="text-color-primary">パスワード</text>
-    <textInput type="text" v-model="password"/>
-    <nb-button class ="login-button">
+<view class="container">
+  <text class="text-color-primary">メールアドレス</text>
+  <textInput type="text" v-model="e-mailadress" />
+  <text class="text-color-primary">パスワード</text>
+  <textInput type="text" v-model="password" />
+  <nb-button class="login-button">
     <text>ログイン</text>
-    </nb-button>
-  </view>
+  </nb-button>
+</view>
 </template>
 
 <script>
-  export default {
-    data: function() {
-      return {
-        e-mailadress: '入力',
-        password: '入力',
-      }
-    },
-    styleObject: {
-      color: 'red',
-      fontSize: '13px'
+export default {
+  data: function() {
+    return {
+      e - mailadress: '入力',
+      password: '入力',
     }
+  },
+  styleObject: {
+    color: 'red',
+    fontSize: '13px'
   }
+}
 </script>
 
 <style>
-  .text-color-primary {
-    color: blue;
-    font-size:20px;
-    padding-bottom:10px;
-    padding-top:10px;
-  }
-  .login-button{
-    margin-top:30px;
-  }
+.text-color-primary {
+  color: blue;
+  font-size: 20px;
+  padding-bottom: 10px;
+  padding-top: 10px;
+}
+
+.login-button {
+  margin-top: 30px;
+}
 </style>
