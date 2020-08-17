cc30d4f82d723d0c6d17c80abecb7a4949c61ddc
nemotoshogo <shota0701nemoto@gmail.com>
2020-08-17 23:26:11 +0900

    コードのフォーマット

--- FrontSide/components/pages/Login/Index.vue
+++ FrontSide/components/pages/Login/Index.vue
@@ -1,46 +1,38 @@
 <template>
-
-<view class="container">
-
-<text class="text-color-primary">メールアドレス</text>
-<textInput type="text" v-model="textInput1"/>
-
-<text class="text-color-primary">パスワード</text>
-<textInput type="text" v-model="textInput2"/>
-
-<nb-button class ="login-button">
-          <text>ログイン</text>
-</nb-button>
-
-
-
-</view>
+  <view class="container">
+    <text class="text-color-primary">メールアドレス</text>
+    <textInput type="text" v-model="e-mailadress"/>
+    <text class="text-color-primary">パスワード</text>
+    <textInput type="text" v-model="password"/>
+    <nb-button class ="login-button">
+    <text>ログイン</text>
+    </nb-button>
+  </view>
 </template>
 
-<style>
-.text-color-primary {
-  color: blue;
-  font-size:20px;
-  padding-bottom:10px;
-  padding-top:10px;
-}
-
-.login-button{
-  margin-top:30px;
-}
-</style>
-
 <script>
-export default {
-  data: function() {
-    return {
-      textInput1: '入力',
-      textInput2: '入力',
+  export default {
+    data: function() {
+      return {
+        e-mailadress: '入力',
+        password: '入力',
+      }
+    },
+    styleObject: {
+      color: 'red',
+      fontSize: '13px'
     }
-  },
-  styleObject: {
-    color: 'red',
-    fontSize: '13px'
   }
-}
 </script>
+
+<style>
+  .text-color-primary {
+    color: blue;
+    font-size:20px;
+    padding-bottom:10px;
+    padding-top:10px;
+  }
+  .login-button{
+    margin-top:30px;
+  }
+</style>
