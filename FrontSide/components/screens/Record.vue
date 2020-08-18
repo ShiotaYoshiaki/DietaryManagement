41160e60800862beb86abc9486798e5b05bce0b0
tanaka-yuu <tanaka07yuu23@gmail.com>
2020-08-18 22:55:51 +0900

    食事記録画面レイアウト編集

--- FrontSide/components/screens/Record.vue
+++ FrontSide/components/screens/Record.vue
@@ -2,13 +2,29 @@
   <nb-container>
     <nb-header>
       <nb-body>
-        <nb-text>食事記録</nb-text>
+        <nb-date-picker
+          :defaultDate="defaultDate"
+          :minimumDate="minimumDate"
+          :maximumDate="maximumDate"
+          :modalTransparent="false"
+          animationType="fade"
+          androidMode="default"
+          :textStyle="stylesObj.dateText"
+          :textContent="date"
+          :onDateChange="setDate"
+          :formatChosenDate="
+            (date) => moment(date).format('YYYY年MM月DD日(ddd)')
+          "
+        />
       </nb-body>
     </nb-header>
 
     <nb-tabs>
       <nb-tab v-for="cook in cooked" :key="cook.key" :heading="cook.message">
-        <Table @transition="(e) => this.props.navigation.navigate(e)" :meal="cook.message"/>
+        <Table
+          @transition="(e) => this.props.navigation.navigate(e)"
+          :meal="cook.message"
+        />
       </nb-tab>
     </nb-tabs>
   </nb-container>
@@ -17,19 +33,40 @@
 <script>
 import Table from "../pages/Record/Table";
 import { MEALS } from "../../constants/meals";
+import React from "react";
+import moment from "moment";
 
 export default {
   components: { Table },
   data: () => {
     return {
+      moment,
       cooked: MEALS,
+      defaultDate: new Date(),
+      minimumDate: new Date("2020-01-01"),
+      maximumDate: new Date("2100-12-31"),
+      chosenDate: new Date(),
+      date: new Date(),
+      stylesObj: {
+        dateText: {
+          color: "#1e90ff",
+        },
+        chenge: {
+          position: "absolute",
+          fontSize: 15,
+          left: 0,
+        },
+      },
     };
   },
   methods: {
     screenTransition(e) {
       this.props.navigation.navigate(e);
     },
-  }
+    setDate(newDate) {
+      this.chosenDate = newDate;
+    },
+  },
 };
 </script>
 
